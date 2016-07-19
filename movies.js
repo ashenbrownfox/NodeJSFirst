b7d9579027adeb77b8e55a43dc2456293fc633ec
ashenbrownfox <newhoopsdynasty@gmail.com>
2016-07-19 14:11:48 -0400

    Experimenting with exporting modules

---
+++ movies.js
@@ -0,0 +1,12 @@
+Module.exports = {
+
+}
+
+function printAvatar(){
+    console.log("Avatar: PG-13")
+}
+function printChap(){
+    console.log("Chappie: R")
+}
+//how to export code
+module.exports.avatar = printAvatar;
