18f7a9e7005f45536672da5da8144e78df848fa8
ashenbrownfox <newhoopsdynasty@gmail.com>
2016-07-19 16:15:28 -0400

    Demonstrating more core modules. Learned about ser Intervals

--- Hello.js
+++ Hello.js
@@ -19,3 +19,53 @@ setTimeout(printBacon, 5000);
 placeanOrder(1);
 placeanOrder(2);
 placeanOrder(3);
+//move test code in here
+//in node everything is a reference
+var me = {
+    favfood: "bacon",
+    favmovie: "hangover"
+};
+//person is merely a reference to me
+var person = me;
+person.favfood = "salad";
+//Only compare the values
+//console.log(19== '19');
+//whenever you want to compare values and types
+//console.log(19=== '19');
+
+//this is a reference to whatever is calling it
+//the default calling context is global
+function doSomethingWorthless(){
+    console.log("\nI am worthless");
+    console.log(this===global);
+}
+
+function User(){
+    this.name="";
+    this.life = 100;
+    this.giveLife = function giveLife(targetPlayer){
+        targetPlayer.life += 1;
+        console.log(this.name + "gave 1 life to "+targetPlayer.name)
+    }
+}
+
+var Ailun = new User(); Ailun.name = "Ailun";
+var Eeyore = new User(); Eeyore.name = "Eeyore";
+
+Ailun.giveLife(Eeyore);
+Eeyore.giveLife(Ailun);
+Eeyore.giveLife(Ailun);
+// console.log("Ailun: "+Ailun.life);
+// console.log("Eeyore: "+Eeyore.life);
+User.prototype.uppercut = function uppercut(targetPlayer){
+    targetPlayer.life -= 2;
+    console.log(this.name + " punched "+targetPlayer.name)
+}
+Ailun.uppercut(Eeyore)
+console.log("Ailun: "+Ailun.life);
+console.log("Eeyore: "+Eeyore.life);
+User.prototype.Magic = 60;
+//importing
+var movies = require("./movies");
+movies.printAvatar();
+console.log(movies.favMovie);
--- app.js
+++ app.js
@@ -1,49 +1,18 @@
-//in node everything is a reference
-var me = {
-    favfood: "bacon",
-    favmovie: "hangover"
-};
-//person is merely a reference to me
-var person = me;
-person.favfood = "salad";
-//Only compare the values
-//console.log(19== '19');
-//whenever you want to compare values and types
-//console.log(19=== '19');
+/** //Demonstrating some examples of core modules **/
 
-//this is a reference to whatever is calling it
-//the default calling context is global
-function doSomethingWorthless(){
-    console.log("\nI am worthless");
-    console.log(this===global);
-}
+var fs = require('fs');
+fs.writeFileSync("lebron.txt", "LeBron is the new king");
+console.log(fs.readFileSync("lebron.txt").toString());
 
-function User(){
-    this.name="";
-    this.life = 100;
-    this.giveLife = function giveLife(targetPlayer){
-        targetPlayer.life += 1;
-        console.log(this.name + "gave 1 life to "+targetPlayer.name)
-    }
-}
+//path module is great when working with file paths
+var path = require('path');
+var homepath = "C:\\\\Users\\\Ashen/////Source\\\Repos\\\\NodeT\\\\app.js";
 
-var Ailun = new User(); Ailun.name = "Ailun";
-var Eeyore = new User(); Eeyore.name = "Eeyore";
+console.log(path.normalize(homepath));
+console.log(path.dirname(homepath));
+console.log(path.basename(homepath));
+console.log(path.extname(homepath));
 
-Ailun.giveLife(Eeyore);
-Eeyore.giveLife(Ailun);
-Eeyore.giveLife(Ailun);
-// console.log("Ailun: "+Ailun.life);
-// console.log("Eeyore: "+Eeyore.life);
-User.prototype.uppercut = function uppercut(targetPlayer){
-    targetPlayer.life -= 2;
-    console.log(this.name + " punched "+targetPlayer.name)
-}
-Ailun.uppercut(Eeyore)
-console.log("Ailun: "+Ailun.life);
-console.log("Eeyore: "+Eeyore.life);
-User.prototype.Magic = 60;
-//importing
-var movies = require("./movies");
-movies.printAvatar();
-console.log(movies.favMovie);
+setInterval(function(){
+   console.log("Cheeseburgers!");
+},2000);
